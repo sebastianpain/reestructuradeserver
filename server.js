3e4a8bfaa59bc338b7bcd25ccca054b8ddf9a0bd
Sebastian Ramirez <sebastianramirezpain@gmail.com>
2023-09-11 20:55:34 -0300

    email

--- server.js
+++ server.js
@@ -2,13 +2,51 @@ import express from 'express'
 import mongoose from 'mongoose'
 import appRouter from './src/routers/app.router.js'
 import CONFIG from './src/config/config.js'
+import nodemailer from 'nodemailer'
 
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({express:true}))
 
+
 app.use('/api',appRouter)
+
+const transpor = nodemailer.createTransport ({
+    service:"gmail",
+    port:587,
+    secure:true,
+    auth:{
+        user:"sebastianramirezpain@gmail.com",
+        pass:"btmlnlaxubovaqdi",
+
+    },
+    tls: {
+        rejectUnauthorized: false
+      }
+}) 
+app.get('/mail',async(req,res)=>{
+    try{
+        const mailParams={
+            from:"sebastianramirezpain@gmail.com",
+            to:"sebastianramirezpain@gmail.com",
+            subject: "Test inicial de envio de mail con un perrito ",
+            html: `<div>
+            Esto es un mail de prueba con un adjunto !!
+            </div>`,
+            attachments:[{
+                filename:"perrito.jpg",
+                path: process.cwd()+'/src/public/perrito.jpg',
+                cid:'perrito'
+            }]
+        }
+        const result = await transpor.sendMail(mailParams)
+        res.send('Mail enviado')
+    } catch(error){
+        console.log(error)
+    }
+})
+
 
 //listen
 mongoose.connect(CONFIG.MONGO_URL)
--- src/routers/app.router.js
+++ src/routers/app.router.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
 
+
 import userRouter from './users/users.router.js';
 import orderRouter from './orders/orders.router.js';
 
 const router = Router()
 
+
 router.use('/users',userRouter);
 router.get('/orders',orderRouter);
+
 
 
 export default router
